5091f5d523a84ffc281e77494670dab12c363efa
Damian Olszewski <damian@ravendb.net>
2025-03-13 13:08:17 +0100

    RavenDB-23292 Add optional label

--- src/Raven.Studio/typescript/components/common/Form.tsx
+++ src/Raven.Studio/typescript/components/common/Form.tsx
@@ -763,3 +763,7 @@ export function FormGroup(props: FormGroupProps) {
 }
 
 export const FormLabel = Form.Label;
+
+export function OptionalLabel() {
+    return <small className="text-muted fw-light">(optional)</small>;
+}
