a99fbd76c062fe11356c0b43bb2031145ab40679
Maksym Smoliński <maksym.smolinski@ravendb.net>
2025-07-18 14:17:13 +0200

    RavenDB-23292 fix (domain step validation and input adjustments) - added placeholder for domain input, integrated error reporting for missing domain, updated domain claim logic to ensure validation runs correctly, and replaced `VerificationCodeInput` with `FormVerificationCodeInput` for consistency in license step

--- src/Raven.Studio/typescript/components/common/Form.stories.tsx
+++ src/Raven.Studio/typescript/components/common/Form.stories.tsx
@@ -13,7 +13,7 @@ import {
     FormRadioToggleWithIcon,
     FormSelect,
     FormSelectCreatable,
-    FormSwitch,
+    FormSwitch, FormVerificationCodeInput,
 } from "./Form";
 import { useEffect } from "react";
 import { withBootstrap5, withStorybookContexts } from "test/storybookTestUtils";
@@ -198,6 +198,10 @@ export function Form({ isDefaultValid }: { isDefaultValid: boolean }) {
                     getPathDependencies={(path: string) => [path]}
                 />
             </FormGroup>
+            <FormGroup>
+                <FormLabel>Verification Code Input</FormLabel>
+                <FormVerificationCodeInput control={control} name="inputVerificationCode" onLastDigitInsertSubmit={() => console.log("last digit inserted")}  />
+            </FormGroup>
         </div>
     );
 }
@@ -219,6 +223,7 @@ const schema = yup.object().shape({
     inputDurationPicker: yup.number().required(),
     inputAceEditor: yup.string().required(),
     inputPath: yup.string().required(),
+    inputVerificationCode: yup.string().required(),
 });
 
 const formResolver = yupResolver(schema);
