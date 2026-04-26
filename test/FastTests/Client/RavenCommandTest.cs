3e344ee7e0e7941566363c33fac2c0b8995d3c13
Ayende Rahien <Ayende@ayende.com>
2026-04-26 10:31:09 +0000

    RavenDB-26452 Add snapshot/fork commands to RavenCommand checked list

--- test/FastTests/Client/RavenCommandTest.cs
+++ test/FastTests/Client/RavenCommandTest.cs
@@ -76,6 +76,7 @@ namespace FastTests.Client
                 "DeleteRevisionsCommand", "ConfigureRevisionsBinCleanerCommand",
                 "GetCollectionRevisionsStatisticsCommand", "AddGenAiCommand","UpdateGenAiCommand", "AddEmbeddingsGenerationCommand",
                 "AddOrUpdateAiAgentOperationCommand","DeleteAiAgentOperationCommand","RunConversationOperationCommand","GetAiAgentOperationCommand","GetConversationMessagesCommand",
+                "CreateConversationSnapshotCommand","ForkConversationCommand","GetConversationSnapshotsCommand","PurgeConversationSnapshotsCommand",
                 "ConfigureAttachmentsRemoteCommand", "GetRemoteAttachmentsConfigurationCommand", "DeleteAttachmentsCommand",
                 "ConfigureSchemaValidationCommand", "GetSchemaValidationCommand", "StartSchemaValidationCommand"
             }.OrderBy(t => t);
